300326d7bad3928fa9c44afb506dea3a55064e82
Espectroxs <125889942+Espectroxs@users.noreply.github.com>
2025-08-29 16:51:25 -0300

    Add files via upload

---
+++ ESPECTRALK1NG.lua
@@ -0,0 +1,12 @@
+local v0=15189 + 975 + ((((5134 -(226 + 1044)) + (1585442 -1221341)) -274396) -(67717 -(32 + 85))) + (190952 -(108427 + 2211)) ;v0=v0 + ((26 + 87) -((983 -(892 + 65)) + 67)) + ((2907 -1688) -((219 -100) + (1830 -833))) ;local v1=1203806 -(87 + 263) ;local v2=1230651 -(67 + 113) ;local v3=5883401 + 2140080 ;if (v2>v1) then print("true");end if (((2 -1) + v3)>v2) then print("obfuscate the conditions!");end print("Clicking [Strings] will completely hide this string!");do local v4=0 + 0 ;local v5;while true do if (v4==(3 -2)) then for v7,v8 in pairs(v5) do if v8 then print("Prime found: "   .. v7 );end end break;end if (v4==(952 -(802 + 150))) then local v6=0;while true do if (v6==(2 -1)) then v4=1 -0 ;break;end if (v6==(0 + 0)) then function sieve_of_eratosthenes(v9) local v10={};for v11=1,v9 do v10[v11]=(998 -(915 + 82))~=v11 ;end for v13=2,math.floor(math.sqrt(v9)) do if v10[v13] then for v14=v13 * v13 ,v9,v13 do v10[v14]=false;end end end return v10;end v5=sieve_of_eratosthenes(1189 -769 );v6=1;end end end end end print("How to obfuscate best?");
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
+
+]]--
